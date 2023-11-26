8f57f0c59d7aeb4583665cc2f6fb6dc42a122d60
kanuki00 <kalevi.toivanen@gmail.com>
2023-11-26 13:37:18 +0200

    added unreal project

---
+++ Source/ComputeShaders2/ComputeShaders2.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ComputeShaders2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ComputeShaders2, "ComputeShaders2" );
