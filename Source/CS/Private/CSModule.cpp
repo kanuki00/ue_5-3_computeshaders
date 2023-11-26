27a6583a72646e2f293fad2ec80aa0eb324ed723
kanuki00 <kalevi.toivanen@gmail.com>
2023-11-26 19:32:11 +0200

    no luck yet

--- Source/CS/Private/CSModule.cpp
+++ Source/CS/Private/CSModule.cpp
@@ -1,7 +1,17 @@
 #include "CSModule.h"
 #include "Modules/ModuleManager.h"
+#include "ShaderCore.h"
 
 IMPLEMENT_MODULE(FCSModule, ModuleTest);
 
-void FCSModule::StartupModule() {}
-void FCSModule::ShutdownModule() {}
+void FCSModule::StartupModule() 
+{
+	const FString ShaderDirectory = FPaths::Combine(FPaths::ProjectDir(), TEXT("Shaders"));
+	const FString CustomShaderDirectory = "/CustomShaders";
+	//AddShaderSourceDirectoryMapping(CustomShaderDirectory, ShaderDirectory);
+}
+
+void FCSModule::ShutdownModule()
+{
+
+}
