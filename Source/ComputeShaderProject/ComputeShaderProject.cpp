b75e89c20870f1d1dc6c41f5464e7e2161a6d571
kanuki00 <kalevi.toivanen@gmail.com>
2024-02-06 13:15:42 +0200

    removed project because i forgot to add gitignore

--- Source/ComputeShaderProject/ComputeShaderProject.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "ComputeShaderProject.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ComputeShaderProject, "ComputeShaderProject" );
